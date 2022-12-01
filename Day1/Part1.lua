5eba139bc23147ef9b53eebc2751fa78248b542d
Luis Delgado <44483231+Luis-D@users.noreply.github.com>
2022-12-01 12:10:16 -0400

    Update Part1.lua

--- Day1/Part1.lua
+++ Day1/Part1.lua
@@ -1,1 +1,14 @@
+acc = 0;
+max = 0;
+for l in io.lines("Input.txt") do
+  if(l == "") then
+  if(acc > max) then
+    max = acc
+    end
+    acc = 0;
+    else
+      acc = acc + (tonumber(l))
+    end
+end
 
+print(max);
